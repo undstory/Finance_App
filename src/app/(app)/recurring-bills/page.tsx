589f331e7ec96f3b8396a19b8e64c63687359c61
undstory <undrul.ozog@gmail.com>
2026-02-05 21:34:40 +0100

    added ui component and block for recurring

--- src/app/(app)/recurring-bills/page.tsx
+++ src/app/(app)/recurring-bills/page.tsx
@@ -1,7 +1,20 @@
+import { Header } from "@/components/ui/Header";
+import Image from "next/image";
+
 export default async function Page() {
     return (
         <div>
-            <h1>Reccuring bills</h1>
+            <Header className="text-[32px] font-bold" title="Reccuring bills" />
+            <div className="flex flex-row mt-(--space-400) gap-[24px]">
+                <div className="w-[33%] text-white p-(--space-300) bg-(--grey-900) flex flex-col rounded-lg">
+                    <Image src="/assets/images/icon-recurring-bills.svg" width={40} height={40} alt="Recurring bills icon" />
+                    <div className="flex flex-col gap-[11px] mt-(--space-400)">
+                        <span>Total bills</span>
+                        <span className="text-[32px] font-bold">$384.25</span>
+                    </div>
+                </div>
+                <div>Hello2</div>
+            </div>
         </div>
     )
 }
--- src/app/(app)/transactions/page.tsx
+++ src/app/(app)/transactions/page.tsx
@@ -1,13 +1,14 @@
 import { Transaction } from "@/types/transactions";
 import TransactionsTable from "./TransactionsTable";
 import { getTransactions } from "@/lib/transactions";
+import { Header } from "@/components/ui/Header";
 
 
 export default async function Page() {
     const transactions: Transaction[] = await getTransactions()
     return (
         <div className="bg-(--beige-100) text-(--grey-900)">
-            <h2 className="text-[32px] font-bold">Transactions</h2>
+            <Header className="text-[32px] font-bold" title="Transactions" />
             <TransactionsTable transactions={transactions} />
         </div>
     )
